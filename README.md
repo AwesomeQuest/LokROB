ded57ef0f6c30285d369a59a200db57ae828a1c6
AwesomeQuest <torfit@gmail.com>
2016-12-06 08:36:30 +0000

    actual final commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # LokROB
+
+This project is one where an aruino is hooked up to a relay and heat sensor.
+
+The arduino receives information from the heat sensor and saves it's starting
+temperature in Celsius and allows electricity to go through the relay, 
+in this case this allows an element heat up water, it does this for 
+temperature*1000 milliseconds. If the sensor detects a temperature higher 
+than it's starting temperature it heats the element that extra amount.
--- control_code/control_code.ino
+++ control_code/control_code.ino
@@ -1,24 +1,29 @@
+//Defines and includes the necessary libraries 
+
 #include "DHT.h"
 
 #define DHTPIN 2
-
-int relayPin = 8;
-float startTemp = 0.0;
 
 #define DHTTYPE DHT22
 
 DHT dht(DHTPIN, DHTTYPE);
 
+//Defines the necessary values needed
+int relayPin = 8;
+float startTemp = 0.0;
 
 void setup() {
-  Serial.begin(9600);
-  Serial.println("DHTxx test!");
-
+  
   dht.begin();
   pinMode(relayPin, OUTPUT);
+  //Gets the starting temperature
   startTemp = dht.readTemperature();
+  //Heats element
   onTimer(startTemp);
 }
+//This function takes the time based on when it started 
+//and compares that time to the current time and heats
+//the element for the appropriate time
 void onTimer(float a) {
   unsigned long timeS = millis();
   unsigned long timeM;
@@ -27,20 +32,23 @@ void onTimer(float a) {
     timeM = millis();
     digitalWrite(relayPin, LOW);
 
-  } while (timeS - (timeM - (a * 100)) > 0);
+  } while (timeS - (timeM - (a * 1000)) > 0);
 
   digitalWrite(relayPin, HIGH);
 
 }
 
-
+//This code checks the current temperature and 
+//compares it to the starting temperature 
+//and activates the relay appropriately
 void loop() {
-  delay(2000);
-
+  delay(10000);
+  //Check current temp
   float t = dht.readTemperature();
-
+  //Compares current temp
   if (t > startTemp) {
     onTimer((t - startTemp));
+    startTemp = t;
   }
 
 
