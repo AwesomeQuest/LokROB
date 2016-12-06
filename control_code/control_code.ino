60331e718fa6e191358a7a685b97db72b10fa8c2
AwesomeQuest <torfit@gmail.com>
2016-12-06 08:14:51 +0000

    last commit

--- control_code/control_code.ino
+++ control_code/control_code.ino
@@ -1,50 +1,47 @@
+#include "DHT.h"
 
-#define DHTTYPE DHT22 
+#define DHTPIN 2
+
+int relayPin = 8;
+float startTemp = 0.0;
+
+#define DHTTYPE DHT22
 
 DHT dht(DHTPIN, DHTTYPE);
 
+
 void setup() {
-  // put your setup code here, to run once:
-dht.begin();
-int relayPin = 3;
-int ledPin
+  Serial.begin(9600);
+  Serial.println("DHTxx test!");
+
+  dht.begin();
+  pinMode(relayPin, OUTPUT);
+  startTemp = dht.readTemperature();
+  onTimer(startTemp);
+}
+void onTimer(float a) {
+  unsigned long timeS = millis();
+  unsigned long timeM;
+
+  do {
+    timeM = millis();
+    digitalWrite(relayPin, LOW);
+
+  } while (timeS - (timeM - (a * 100)) > 0);
+
+  digitalWrite(relayPin, HIGH);
+
 }
 
+
 void loop() {
-  // put your main code here, to run repeatedly:
   delay(2000);
 
   float t = dht.readTemperature();
 
-  switch(t){
-    case (t<25):
-
-    digitalWrite(relayPin, HIGH);
-    digitalWrite(ledPin, HIGH);
-
-    break;
-
-    case (t>25):
-
-    digitalWrite(relayPin, LOW);
-    digitalWrite(ledPin, LOW);
-    
-    break;
-
-    default:
-
-    for(int i = 0; i < 10; i++){
-          
-    digitalWrite(relayPin, HIGH);
-    digitalWrite(ledPin, HIGH);
-    delay(500);
-    digitalWrite(relayPin, LOW);
-    digitalWrite(ledPin, LOW);
-    delay(500);
-
-
-    }
-
-    break;
+  if (t > startTemp) {
+    onTimer((t - startTemp));
   }
+
+
 }
